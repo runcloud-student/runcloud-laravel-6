31d9993fe1321d21d03de4e76e37e9a4b08be978
Abdul Hadi <hadian90@gmail.com>
2019-02-07 14:36:57 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    RunCloud
+                    RunCloud Test
                 </div>
 
                 <div class="links">
